20cf6e45f89428dffb8ba22ab30e5998999fc2c3
Jakkawad <jakkawadchaiplee@Jakkawads-MacBook-Pro.local>
2016-06-04 09:23:31 +0700

    ProductDetail V3-4
    ProductDetail V3-4

---
+++ A2SLayoutsK3/Page4ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Page4ViewController.swift
+//  A2SLayoutsK3
+//
+//  Created by Jakkawad Chaiplee on 6/3/2559 BE.
+//  Copyright © 2559 All2Sale. All rights reserved.
+//
+
+import UIKit
+
+class Page4ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
